faa6e28146665d7ea76a19e978f4fe2baa5a124e
João Sipauba Silva Pessoa <joaosipauba@hotmail.com>
2022-10-12 16:46:32 -0300

    fix: validação para o campo de workload

--- src/main/java/com/api/coursesmanager/dtos/CourseDto.java
+++ src/main/java/com/api/coursesmanager/dtos/CourseDto.java
@@ -2,11 +2,14 @@ package com.api.coursesmanager.dtos;
 
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 
 public class CourseDto {
     @NotBlank(message = "CourseName cannot be empty.")
     private String courseName;
+    @NotNull(message = "CourseWorkload cannot be empty.")
     @Min(value = 10, message = "CourseWorkload should be greater than 10.")
     private Number courseWorkload;
 
