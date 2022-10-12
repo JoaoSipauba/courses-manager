0dff10e3ba400ff6975ff46189e6f3ba03e217ea
João Sipauba Silva Pessoa <joaosipauba@hotmail.com>
2022-10-12 15:34:28 -0300

    configuração inicial

---
+++ src/main/java/com/api/coursesmanager/CoursesManagerApplication.java
@@ -0,0 +1,13 @@
+package com.api.coursesmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursesManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoursesManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/api/coursesmanager/CoursesManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.coursesmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursesManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
