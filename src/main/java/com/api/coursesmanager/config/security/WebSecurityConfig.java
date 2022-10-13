1731ac21e831be191eaec8c5cf3caa205472b320
João Sipauba Silva Pessoa <joaosipauba@hotmail.com>
2022-10-13 17:00:38 -0300

    refactor: importações não utilizadas

--- src/main/java/com/api/coursesmanager/config/security/WebSecurityConfig.java
+++ src/main/java/com/api/coursesmanager/config/security/WebSecurityConfig.java
@@ -1,6 +1,5 @@
 package com.api.coursesmanager.config.security;
 
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
