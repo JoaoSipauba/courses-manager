6878bd953e2c564a39db9bb0d15de7cc1bebae98
João Sipauba Silva Pessoa <joaosipauba@hotmail.com>
2022-10-13 16:08:34 -0300

    refactor: valores do keycloak na application properties

--- src/main/java/com/api/coursesmanager/services/AuthService.java
+++ src/main/java/com/api/coursesmanager/services/AuthService.java
@@ -1,6 +1,7 @@
 package com.api.coursesmanager.services;
 
 import com.api.coursesmanager.dtos.AuthDto;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.util.LinkedMultiValueMap;
@@ -9,18 +10,22 @@ import org.springframework.web.client.RestTemplate;
 
 @Service
 public class AuthService {
+    @Value("${keycloak.client-id}")
+    private String clientId;
+    @Value("${keycloak.client-secret}")
+    private String clientSecret;
+    @Value("${keycloak.auth-url}")
+    private String authUrl;
 
     public Object authenticate(AuthDto authDto){
-        String url = "http://localhost:8080/realms/courses-manager/protocol/openid-connect/token";
-
         RestTemplate restTemplate = new RestTemplate();
 
         HttpHeaders headers = new HttpHeaders();
         headers.add("Content-Type", MediaType.APPLICATION_FORM_URLENCODED.toString());
 
         MultiValueMap<String, String> requestBody = new LinkedMultiValueMap();
-        requestBody.add("client_id", "web-app");
-        requestBody.add("client_secret", "IWBER6HtNogkat4GccxBWIcYuD8q8hKe");
+        requestBody.add("client_id", clientId);
+        requestBody.add("client_secret", clientSecret);
         requestBody.add("grant_type", "password");
         requestBody.add("username", authDto.getUsername());
         requestBody.add("password", authDto.getPassword());
@@ -28,7 +33,7 @@ public class AuthService {
         HttpEntity formEntity = new HttpEntity(requestBody, headers);
 
         ResponseEntity<Object> response =
-                restTemplate.exchange(url, HttpMethod.POST, formEntity, Object.class);
+                restTemplate.exchange(authUrl, HttpMethod.POST, formEntity, Object.class);
         return response.getBody();
     }
 }
