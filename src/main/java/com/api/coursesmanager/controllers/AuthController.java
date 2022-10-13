6d06fd86448a75989270d8ae23930c5f59d2f329
João Sipauba Silva Pessoa <joaosipauba@hotmail.com>
2022-10-13 15:58:36 -0300

    feat: login com username e password

--- src/main/java/com/api/coursesmanager/controllers/AuthController.java
+++ src/main/java/com/api/coursesmanager/controllers/AuthController.java
@@ -1,12 +1,12 @@
 package com.api.coursesmanager.controllers;
 
+import com.api.coursesmanager.dtos.AuthDto;
 import com.api.coursesmanager.services.AuthService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/authentication")
@@ -18,7 +18,7 @@ public class AuthController {
     }
 
     @PostMapping
-    public ResponseEntity<Object> authentication(){
-        return ResponseEntity.status(HttpStatus.OK).body(authService.authenticate());
+    public ResponseEntity<Object> authentication(@RequestBody @Valid AuthDto authDto){
+        return ResponseEntity.status(HttpStatus.OK).body(authService.authenticate(authDto));
     }
 }
--- src/main/java/com/api/coursesmanager/services/AuthService.java
+++ src/main/java/com/api/coursesmanager/services/AuthService.java
@@ -1,5 +1,6 @@
 package com.api.coursesmanager.services;
 
+import com.api.coursesmanager.dtos.AuthDto;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.util.LinkedMultiValueMap;
@@ -9,7 +10,7 @@ import org.springframework.web.client.RestTemplate;
 @Service
 public class AuthService {
 
-    public Object authenticate(){
+    public Object authenticate(AuthDto authDto){
         String url = "http://localhost:8080/realms/courses-manager/protocol/openid-connect/token";
 
         RestTemplate restTemplate = new RestTemplate();
@@ -17,10 +18,12 @@ public class AuthService {
         HttpHeaders headers = new HttpHeaders();
         headers.add("Content-Type", MediaType.APPLICATION_FORM_URLENCODED.toString());
 
-        MultiValueMap<String, String> requestBody = new LinkedMultiValueMap<String, String>();
+        MultiValueMap<String, String> requestBody = new LinkedMultiValueMap();
         requestBody.add("client_id", "web-app");
         requestBody.add("client_secret", "IWBER6HtNogkat4GccxBWIcYuD8q8hKe");
-        requestBody.add("grant_type", "client_credentials");
+        requestBody.add("grant_type", "password");
+        requestBody.add("username", authDto.getUsername());
+        requestBody.add("password", authDto.getPassword());
 
         HttpEntity formEntity = new HttpEntity(requestBody, headers);
 
